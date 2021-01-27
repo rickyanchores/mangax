107100ad81faacf074f9e50b0c08b04b4a7f7da8
RickyAnchores <ricky23anchores@gmail.com>
2021-01-27 22:14:09 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import Nav from './COMPONENTS/NAV/Nav';
+
+
+
+
+
+//IMPORT THIS FOR REACT ROUTER DOM
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./COMPONENTS/HOME/Home";
+import About from "./COMPONENTS/ABOUT/About";
+import Shop from "./COMPONENTS/SHOP/Shop";
+
+
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/shop" exact component={Shop}/>
+        <Route path="/about" exact component={About}/>
+      </Switch>
+    </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ src/COMPONENTS/NAV/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className="logo"><h1>mangax</h1></div>
+            <ul className="nav-links">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/shop">Shop</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
+
